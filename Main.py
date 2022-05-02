2219539c915e0ae0d2264ef7ec5b9c6f0a77b1ea
6eba610ia9 <Sebastiansebastia6@gmail.com>
2022-05-03 00:26:01 +0300

    Am adăugat detecția frazelor de salut

--- Main.py
+++ Main.py
@@ -1,20 +1,48 @@
 import discord
 import emoji
 import time
-TOKEN = 'OTcwMjAyMjM5NDc2MDcyNDY4.Ym4hPw.4CIJNUqxzAPjAIdpOSKopgLTc70'
+TOKEN = ''
 
 client = discord.Client()
 
-bad_words = []
+#variabile
 zet = "https://www.instagram.com/justzetu/"
 seby = "https://www.instagram.com/sebasti4n.exe/"
 
+
+#mesaj logare bot - NU EDITA NIMIC!
 @client.event
 async def on_ready():
     print('Ne-am logat ca {0.user}'.format(client))
+
+
+
+#Fraze de bun venit
 @client.event
 async def on_message(message):
-    if message.content == 'developers':
-        await message.channel.send(f"{zet} \n {seby}")
-        
+    if message.content == 'Salut':
+        await message.channel.send("Bunăăă ♥")
+    if message.content == 'SALUT':
+        await message.channel.send("Bunăăă ♥")
+    if message.content == 'Hei':
+        await message.channel.send("Bunăăă ♥")
+    if message.content == 'HEI':
+        await message.channel.send("Bunăăă ♥")
+    if message.content == 'Hey':
+        await message.channel.send("Bunăăă ♥")
+    if message.content == 'HEY':
+        await message.channel.send("Bunăăă ♥")
+    if message.content == 'salut':
+        await message.channel.send("Bunăăă ♥")
+    if message.content == 'hei':
+        await message.channel.send("Bunăăă ♥")
+    if message.content == 'hey':
+        await message.channel.send("Bunăăă ♥")
+
+
+#comenzi bot
+    if message.content == '*developers':
+        await message.channel.send(f"{zet, emoji.heart} \n {seby, emoji.heart}")
+
+
 client.run(TOKEN)
